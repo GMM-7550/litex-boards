4f9f3b46a8292c4b69703a885aec4d6625ebc0dd
Anton Kuzmin <ak@gmm7550.dev>
2026-04-17 18:43:51 +0200

    Create library of vendor primitives and instantiate GateMate SerDes -- only register interface is connected now

--- litex_boards/targets/gmm7550/usb3_test.vhd
+++ litex_boards/targets/gmm7550/usb3_test.vhd
@@ -1,8 +1,8 @@
 library ieee;
 use ieee.std_logic_1164.all;
 
--- use work.gatemate.all;
-use work.all;
+library cc;
+use cc.gatemate.all;
 
 entity usb3_test is
   port (
@@ -37,7 +37,7 @@ architecture rtl of usb3_test is
   signal regfile_rdy : std_logic;
 begin
 
-  i_wb_reg_adapter: entity wb_ccserdes_reg
+  i_wb_reg_adapter: entity work.wb_ccserdes_reg
     port map (
       wb_clk_i => wb_clk_i,
       wb_rst_i => wb_rst_i,
@@ -60,18 +60,62 @@ begin
       regfile_rdy_i => regfile_rdy
       );
 
-  -- Dummy, before SerDes is connected
-  regfile_rdy <= '1';
-  regfile_dat_r2wb <= x"aa55";
-
-  -- i_serdes: component cc_serdes
+  i_serdes: component cc_serdes
   --   -- generic map (
   --   --   )
-  --   port map (
+    port map (
   --     ser_tx_p => ser_tx_p,
   --     ser_tx_n => ser_tx_n,
   --     ser_rx_p => ser_rx_p,
-  --     ser_rx_n => ser_rx_n
-  --     );
+  --     ser_rx_n => ser_rx_n,
+
+      tx_reset_i          => '0',
+      tx_pcs_reset_i      => '0',
+      tx_pma_reset_i      => '0',
+      tx_power_down_n_i   => '0',
+      tx_clk_i            => '0',
+      tx_polarity_i       => '0',
+      tx_prbs_sel_i       => (others => '0'),
+      tx_prbs_force_err_i => '0',
+      tx_8b10b_en_i       => '1',
+      tx_8b10b_bypass_i   => (others => '0'),
+      tx_char_is_k_i      => (others => '0'),
+      tx_char_dispmode_i  => (others => '0'),
+      tx_char_dispval_i   => (others => '0'),
+      tx_elec_idle_i      => '0',
+      tx_detect_rx_i      => '0',
+      tx_data_i           => (others => '0'),
+
+      rx_reset_i          => '0',
+      rx_pma_reset_i      => '0',
+      rx_eqa_reset_i      => '0',
+      rx_cdr_reset_i      => '0',
+      rx_pcs_reset_i      => '0',
+      rx_buf_reset_i      => '0',
+      rx_power_down_n_i   => '0',
+      rx_clk_i            => '0',
+      rx_polarity_i       => '0',
+      rx_prbs_sel_i       => (others => '0'),
+      rx_prbs_cnt_reset_i => '0',
+      rx_8b10b_en_i       => '0',
+      rx_8b10b_bypass_i   => (others => '0'),
+      rx_en_ei_detector_i => '0',
+      rx_comma_detect_en_i=> '0',
+      rx_slide_i          => '0',
+      rx_mcomma_align_i   => '0',
+      rx_pcomma_align_i   => '0',
+
+      pll_reset_i         => '0',
+      loopback_i          => (others =>'0'),
+
+      regfile_clk_i  => regfile_clk,
+      regfile_en_i   => regfile_en,
+      regfile_we_i   => regfile_we,
+      regfile_addr_i => regfile_adr,
+      regfile_di_i   => regfile_dat_wb2r,
+      regfile_mask_i => regfile_msk,
+      regfile_do_o   => regfile_dat_r2wb,
+      regfile_rdy_o  => regfile_rdy
+      );
 
 end architecture rtl;
